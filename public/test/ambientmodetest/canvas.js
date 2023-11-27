49372eddc3c36a7d56325a8a5eb27b0146385b4c
zydezu <kinomaxxing@gmail.com>
2023-11-27 22:51:45 +0000

    update files

---
+++ public/test/ambientmodetest/canvas.js
@@ -0,0 +1,49 @@
+const video = document.getElementById("ambientvideo");
+const canvas = document.getElementById("ambientcanvas");
+const ctx = canvas.getContext("2d");
+    
+let step; // Keep track of requestAnimationFrame id
+
+ctx.filter = "blur(20px)";
+
+const draw = () => {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+};
+
+const drawLoop = () => {
+    draw();
+    step = window.requestAnimationFrame(drawLoop);
+};
+
+const drawPause = () => {
+    window.cancelAnimationFrame(step);
+    step = undefined;
+};  
+
+const init = () => {
+    video.addEventListener("loadeddata", draw, false);
+    video.addEventListener("seeked", draw, false);
+    video.addEventListener("play", drawLoop, false);
+    video.addEventListener("pause", drawPause, false);
+    video.addEventListener("ended", drawPause, false);
+};
+  
+const cleanup = () => {
+    video.removeEventListener("loadeddata", draw);
+    video.removeEventListener("seeked", draw);
+    video.removeEventListener("play", drawLoop);
+    video.removeEventListener("pause", drawPause);
+    video.removeEventListener("ended", drawPause);
+};
+  
+const initcheck = () => {
+    if (navigator.userAgent.indexOf("Chrome") > -1){
+        init()
+    } else {
+        ctx.filter = "blur(5px)";
+        setInterval(draw, 240) // why does firefox lag like hell
+    }
+}
+
+window.addEventListener("load", initcheck);
+window.addEventListener("unload", cleanup);
