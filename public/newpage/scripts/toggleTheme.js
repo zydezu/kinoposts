38e1c2ad2869d418576f5f055ab8ef6743e87e23
Kino <hasmaj16@gmail.com>
2023-12-11 14:43:06 +0000

    update files

--- public/newpage/scripts/toggleTheme.js
+++ public/newpage/scripts/toggleTheme.js
@@ -2,15 +2,31 @@ const toggleTheme = document.getElementById("toggleThemeButton")
 
 const meta = document.querySelector('meta[name="color-scheme"]');
 const root = document.querySelector(":root");
-let currentColorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+
+let currentColorScheme;
+if (localStorage.getItem("currentTheme")) {
+    currentColorScheme = localStorage.getItem("currentTheme");
+    meta.content = currentColorScheme;
+    root.classList.toggle("dark", currentColorScheme === 'dark');
+    root.classList.toggle("light", currentColorScheme === 'light');
+} else {
+    currentColorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+}
 
 toggleTheme.addEventListener("click", () => {
-    currentColorScheme = currentColorScheme === 'light' ? 'dark' : 'light';
-    meta.content = currentColorScheme;
     changeTheme();
 })
 
 function changeTheme() {
+    currentColorScheme = currentColorScheme === 'light' ? 'dark' : 'light';
+    meta.content = currentColorScheme;
     root.classList.toggle("dark", currentColorScheme === 'dark');
     root.classList.toggle("light", currentColorScheme === 'light');
+    localStorage.setItem("currentTheme", currentColorScheme);
 }
+
+const toggleButton = document.getElementById("themeToggle");
+toggleButton.addEventListener("click", () => {
+    toggleButton.classList.toggle("active")
+    changeTheme()
+})
