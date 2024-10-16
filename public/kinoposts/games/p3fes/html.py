b86f0c042545f3cb05b792126bebae79af8f319b
zydezu <kinomaxxing@gmail.com>
2024-10-16 19:47:18 +0100

    newss

--- public/kinoposts/games/p3fes/html.py
+++
@@ -1,7 +0,0 @@
-with open('tmp.txt') as f:
-    lines = f.readlines()
-    newlines = []
-    for line in lines:
-        newlines.append('''<img loading="lazy" class="rounding image" src="{0}"><br/>\n'''.format(line.replace('\n','')))
-    with open('new.txt', "w") as w:
-        w.writelines(newlines)
